4e8236a65ebbf4ab4f12d4238c88ef568d57a1f3
Georgi Iliev <georgi.iliev533@outlook.com>
2023-09-25 22:54:10 +0300

    API endpoint to reset password

--- app/api.py
+++ app/api.py
@@ -6,6 +6,8 @@ from flask_security.utils import hash_password
 from app.db import db
 from app.auth import user_datastore
 
+from app.models import User
+
 api = Blueprint('api', __name__)
 
 
@@ -23,6 +25,16 @@ def update():
             username=request.form['username'],
             password=hash_password(request.form['password'])
         )
+        db.session.commit()
+        return {'status': 'success'}
+    return {'status': 'invalid authenticator'}
+
+
+@api.route('/resetpass', methods=['POST'])
+def reset_password():
+    if os.getenv('FLASK_API_SECRET_KEY') == request.headers.get('Authorization').split(' ')[1]:
+        user = User.query.filter_by(username=request.form['username']).first()
+        user.password = hash_password(request.form['password'])
         db.session.commit()
         return {'status': 'success'}
     return {'status': 'invalid authenticator'}
