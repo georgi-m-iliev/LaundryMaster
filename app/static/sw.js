0cb85629217beef271248c2d916294537a5ee29c
Georgi Iliev <georgi.iliev533@outlook.com>
2023-10-14 21:03:46 +0300

    Add event on click of push

--- app/static/sw.js
+++ app/static/sw.js
@@ -62,3 +62,19 @@ self.addEventListener('push', function (event) {
         self.registration.showNotification(title, options)
     );
 });
+
+self.addEventListener('notificationclick', event => {
+    const rootUrl = new URL('/', location).href;
+    event.notification.close();
+    // Enumerate windows, and call window.focus(), or open a new one.
+    event.waitUntil(
+        clients.matchAll().then(matchedClients => {
+            for (let client of matchedClients) {
+                if (client.url === rootUrl) {
+                    return client.focus();
+                }
+            }
+            return clients.openWindow("/");
+        })
+    );
+});
