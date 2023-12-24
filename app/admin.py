b8584208686eda7a280a159b5155ecb2b6487a44
Georgi Iliev <georgi.iliev533@outlook.com>
2023-12-24 23:27:06 +0200

    Changes for admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, request, render_template
-from flask_security import login_required
-from flask_security.utils import hash_password
+from flask_security import roles_required
 
 from app.db import db
 from app.auth import user_datastore
@@ -9,5 +8,6 @@ admin = Blueprint('admin', __name__)
 
 
 @admin.route('/', methods=['GET'])
+@roles_required('admin')
 def test():
     return render_template('admin/index.html')
