deb428bdb67da07d3028520ab675c79e5efd19dc
Georgi Iliev <georgi.iliev533@outlook.com>
2024-02-11 02:17:36 +0200

    Refactor and fix tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,11 +1,17 @@
 import pytest
 
 
-def test_login(client, app):
-    assert client.get('/login').status_code == 200
-    response = client.post(
-        '/login', data={'username': 'ivan', 'password': 'password', 'login': ''}
+def login(client, app, username, password):
+    """ Login helper function """
+    return client.post(
+        '/login', data={'username': username, 'password': password, 'login': ''}
     )
+
+
+def test_login(client, app):
+    """ Testing login works with correct credentials """
+    assert client.get('/login').status_code == 200
+    response = login(client, app, 'ivan', 'password')
 
     # there should be no alerts for successful login
     assert str(response.data).find('alert') == -1
@@ -13,9 +19,19 @@ def test_login(client, app):
     assert response.headers['Location'] == '/'
 
 
+def test_login_failed(client, app):
+    """ Testing login fails with incorrect credentials """
+    assert client.get('/login').status_code == 200
+    response = login(client, app, 'ivan', 'wrong_password')
+
+    # check that the user is not redirected to the login page, login wasn't successful
+    assert 'Location' not in response.headers
+
+
 def test_logout(client, app):
-    # login using the test_login function
-    test_login(client, app)
+    """ Testing logout """
+    # login using the login helper function
+    login(client, app, 'ivan', 'password')
     # make sure the user is logged in
     index_request = client.get('/')
     assert index_request.status_code == 200
