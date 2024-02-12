eb9dcba19a1e4a9cd2481080a16e903de3058225
Georgi Iliev <georgi.iliev533@outlook.com>
2024-02-13 00:57:22 +0200

    Finish testing functions

--- tests/data.sql
+++ tests/data.sql
@@ -3,5 +3,5 @@ INSERT INTO users(id, email, username, password, first_name, last_login, active,
     (2, 'andrei@test.com', 'andrei', '$2b$12$7XSJWt2rpmHUiGcTEsZpVON.DuyoWqwgYECdSGIPtwg4/OVSMjq46', 'Andrei', NULL, TRUE, '8a512dcdf7414099a3fecfe1f219987a');
 INSERT INTO user_settings(id, user_id, terminate_cycle_on_usage, launch_candy_on_cycle_start) VALUES(1, 1, FALSE, TRUE);
 INSERT INTO roles(id, name) VALUES (1, 'user'), (2, 'admin'), (3, 'room_owner');
-INSERT INTO roles_users(user_id, role_id) VALUES (1, 1);
+INSERT INTO roles_users(user_id, role_id) VALUES (1, 1), (2, 1), (2, 3);
 INSERT INTO washing_machine(currentkwh, costperkwh, public_wash_cost, candy_device_id, candy_appliance_id) VALUES(17.0599,0.20,10.00,'b461fca47c039e37','52528914-64ef-4fc8-a851-3006b0360639');
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -7,7 +7,7 @@ from flask import request
 import app.functions
 from app.db import db
 from app.functions import *
-from app.models import User, WashingCycle, WashingCycleSplit
+from app.models import User, WashingCycle, WashingCycleSplit, ScheduleEvent
 from app.forms import SplitCycleForm
 
 
@@ -16,7 +16,7 @@ from app.forms import SplitCycleForm
 @patch('app.functions.CeleryTask')
 def test_start_cycle(mock_celery_task, mock_flash, mock_trigger_relay, app):
     """ Testing start cycle function in normal conditions. """
-    with app.app_context():
+    with app.test_request_context():
         user = User.query.first()
         mock_trigger_relay.return_value = 200
 
@@ -35,7 +35,7 @@ def test_start_cycle(mock_celery_task, mock_flash, mock_trigger_relay, app):
 @patch('app.functions.CeleryTask')
 def test_start_cycle_trigger_fail(mock_celery_task, mock_flash, mock_trigger_relay, app):
     """ Testing start cycle function with relay trigger fail. """
-    with app.app_context():
+    with app.test_request_context():
         user = User.query.first()
         mock_trigger_relay.return_value = 500
 
@@ -54,7 +54,7 @@ def test_start_cycle_trigger_fail(mock_celery_task, mock_flash, mock_trigger_rel
 @patch('app.functions.CeleryTask')
 def test_start_cycle_already_running(mock_celery_task, mock_flash, app):
     """ Testing start cycle function when a cycle is already running. """
-    with app.app_context():
+    with app.test_request_context():
         user = User.query.first()
         db.session.add(WashingCycle(user_id=user.id, start_timestamp=datetime.datetime.now()))
         db.session.commit()
@@ -75,14 +75,14 @@ def test_start_cycle_already_running(mock_celery_task, mock_flash, app):
 @patch('app.functions.flash')
 def test_stop_cycle(mock_flash, mock_update_consumption, mock_trigger_relay, app):
     """ Testing stop cycle function in normal conditions. """
-    with app.app_context():
+    with app.test_request_context():
         user = User.query.first()
         cycle = WashingCycle(user_id=user.id, start_timestamp=datetime.datetime.now(), startkwh=12)
         db.session.add(cycle)
         db.session.commit()
         mock_trigger_relay.return_value = 200
 
-        with patch.object(cycle, 'notification_task', return_value='task id') as mock_notification_task:
+        with patch.object(WashingCycle, 'notification_task', return_value='task id') as mock_notification_task:
             stop_cycle(user)
             assert mock_notification_task.terminate.called
 
@@ -98,7 +98,7 @@ def test_stop_cycle(mock_flash, mock_update_consumption, mock_trigger_relay, app
 @patch('app.functions.flash')
 def test_stop_cycle_wrong_user(mock_flash, app):
     """ Testing stop cycle function when another user tries to stop a cycle. """
-    with app.app_context():
+    with app.test_request_context():
         current_user = User.query.filter_by(username='ivan').first()
         active_user = User.query.filter_by(username='andrei').first()
         cycle = WashingCycle(user_id=active_user.id, start_timestamp=datetime.datetime.now())
@@ -121,7 +121,7 @@ def test_stop_cycle_wrong_user(mock_flash, app):
 @patch('app.functions.flash')
 def test_stop_cycle_trigger_fail(mock_flash, mock_trigger_relay, app):
     """ Testing stop cycle function in normal conditions. """
-    with app.app_context():
+    with app.test_request_context():
         user = User.query.first()
         cycle = WashingCycle(user_id=user.id, start_timestamp=datetime.datetime.now(), startkwh=12)
         db.session.add(cycle)
@@ -142,7 +142,7 @@ def test_stop_cycle_trigger_fail(mock_flash, mock_trigger_relay, app):
 
 def test_update_cycle_available(app):
     """ Testing the update cycle function when there is no active cycle. """
-    with app.app_context():
+    with app.test_request_context():
         user = User.query.first()
 
         result = update_cycle(user)
@@ -153,7 +153,7 @@ def test_update_cycle_available(app):
 
 def test_update_cycle_active_cycle(app):
     """ Testing the update cycle function when current user has an active cycle. """
-    with app.app_context():
+    with app.test_request_context():
         user = User.query.first()
         cycle = WashingCycle(user_id=user.id, start_timestamp=datetime.datetime.now())
         db.session.add(cycle)
@@ -168,7 +168,7 @@ def test_update_cycle_active_cycle(app):
 
 def test_update_cycle_washing_machine_busy(app):
     """ Testing the update cycle function when washing machine is busy. """
-    with app.app_context():
+    with app.test_request_context():
         current_user = User.query.filter_by(username='ivan').first()
         active_user = User.query.filter_by(username='andrei').first()
         cycle = WashingCycle(user_id=active_user.id, start_timestamp=datetime.datetime.now())
@@ -186,7 +186,7 @@ def test_update_cycle_washing_machine_busy(app):
 @patch('app.functions.flash')
 def test_split_cycle(mock_flash, mock_send_push, app):
     """ Testing the split cycle function when all conditions are satisfied. """
-    with app.app_context():
+    with app.test_request_context():
         current_user = User.query.filter_by(username='ivan').first()
         other_user = User.query.filter_by(username='andrei').first()
         cycle = WashingCycle(
@@ -215,7 +215,7 @@ def test_split_cycle(mock_flash, mock_send_push, app):
 @patch('app.functions.flash')
 def test_split_cycle_not_owned_by_user(mock_flash, mock_send_push, app):
     """Testing the split cycle function when the cycle is not owned by the current user. """
-    with app.app_context():
+    with app.test_request_context():
         current_user = User.query.filter_by(username='ivan').first()
         other_user = User.query.filter_by(username='andrei').first()
         cycle = WashingCycle(
@@ -241,7 +241,7 @@ def test_split_cycle_not_owned_by_user(mock_flash, mock_send_push, app):
 @patch('app.functions.flash')
 def test_split_cycle_already_paid(mock_flash, mock_send_push, app):
     """Testing the split cycle function when the cycle is not owned by the current user. """
-    with app.app_context():
+    with app.test_request_context():
         current_user = User.query.filter_by(username='ivan').first()
         other_user = User.query.filter_by(username='andrei').first()
         cycle = WashingCycle(
@@ -290,3 +290,178 @@ def test_split_cycle_already_paid_split(mock_flash, mock_redirect, mock_send_pus
         assert mock_send_push.called is False
         assert mock_redirect.called
         mock_flash.assert_called_with('You cannot split cycles that have paid splits', category='toast-error')
+
+
+@patch('app.functions.send_push_to_user')
+@patch('app.functions.CeleryTask')
+@patch('app.functions.redirect')
+@patch('app.functions.flash')
+def test_schedule_update_event(mock_flash, mock_redirect, mock_celery_task, mock_send_push, app):
+    """ Testing the update schedule event function in normal conditions. """
+    with app.test_request_context('/'):
+        current_user = User.query.filter_by(username='ivan').first()
+        event = ScheduleEvent(
+            timestamp=datetime.datetime.now() - datetime.timedelta(hours=2),
+            start_timestamp=datetime.datetime.now() + datetime.timedelta(hours=4),
+            end_timestamp=datetime.datetime.now() + datetime.timedelta(hours=6),
+            user_id=current_user.id
+        )
+        db.session.add(event)
+        db.session.commit()
+        db.session.refresh(event)
+
+        new_start_timestamp = event.start_timestamp + datetime.timedelta(hours=1)
+        new_end_timestamp = event.end_timestamp + datetime.timedelta(hours=1)
+
+        with patch.object(ScheduleEvent, 'notification_task', return_value='task id') as mock_notification_task:
+            schedule_update_event(
+                event_id=event.id,
+                start_timestamp=new_start_timestamp,
+                end_timestamp=new_end_timestamp,
+                user=current_user
+            )
+            assert mock_notification_task.terminate.called
+
+        db.session.refresh(event)
+
+        assert event.start_timestamp == new_start_timestamp
+        assert event.end_timestamp == new_end_timestamp
+        assert mock_celery_task.start_schedule_notification_task.called
+        assert mock_send_push.call_count == 1
+
+
+@patch('app.functions.send_push_to_user')
+@patch('app.functions.CeleryTask')
+@patch('app.functions.redirect')
+@patch('app.functions.flash')
+def test_schedule_update_event_invalid_id(mock_flash, mock_redirect, mock_celery_task, mock_send_push, app):
+    """ Testing the update schedule event function with invalid event id. """
+    with app.test_request_context('/'):
+        current_user = User.query.filter_by(username='ivan').first()
+
+        new_start_timestamp = datetime.datetime.now() + datetime.timedelta(hours=1)
+        new_end_timestamp = new_start_timestamp + datetime.timedelta(hours=3)
+
+        with patch.object(ScheduleEvent, 'notification_task', return_value='task id') as mock_notification_task:
+            schedule_update_event(
+                event_id=1,
+                start_timestamp=new_start_timestamp,
+                end_timestamp=new_end_timestamp,
+                user=current_user
+            )
+            assert not mock_notification_task.terminate.called
+
+        mock_flash.assert_called_with('Event not found', category='toast-error')
+        assert mock_redirect.called
+        assert not mock_celery_task.start_schedule_notification_task.called
+        assert mock_send_push.call_count == 0
+
+
+@patch('app.functions.send_push_to_user')
+@patch('app.functions.CeleryTask')
+@patch('app.functions.redirect')
+@patch('app.functions.flash')
+def test_schedule_update_event_wrong_user(mock_flash, mock_redirect, mock_celery_task, mock_send_push, app):
+    """ Testing the update schedule event function with wrong user. """
+    with app.test_request_context('/'):
+        current_user = User.query.filter_by(username='ivan').first()
+        event_user = User.query.filter_by(username='andrei').first()
+        event = ScheduleEvent(
+            timestamp=datetime.datetime.now() - datetime.timedelta(hours=2),
+            start_timestamp=datetime.datetime.now() + datetime.timedelta(hours=4),
+            end_timestamp=datetime.datetime.now() + datetime.timedelta(hours=6),
+            user_id=event_user.id
+        )
+        db.session.add(event)
+        db.session.commit()
+        db.session.refresh(event)
+
+        new_start_timestamp = event.start_timestamp + datetime.timedelta(hours=1)
+        new_end_timestamp = event.end_timestamp + datetime.timedelta(hours=1)
+
+        with patch.object(ScheduleEvent, 'notification_task', return_value='task id') as mock_notification_task:
+            schedule_update_event(
+                event_id=event.id,
+                start_timestamp=new_start_timestamp,
+                end_timestamp=new_end_timestamp,
+                user=current_user
+            )
+            assert not mock_notification_task.terminate.called
+
+        db.session.refresh(event)
+
+        mock_flash.assert_called_with('You can only edit your own events', category='toast-error')
+        assert mock_redirect.called
+        assert not mock_celery_task.start_schedule_notification_task.called
+        assert mock_send_push.call_count == 0
+
+
+@patch('app.functions.send_push_to_user')
+@patch('app.functions.redirect')
+@patch('app.functions.flash')
+def test_schedule_delete_event(mock_flash, mock_redirect, mock_send_push, app):
+    """ Testing the delete schedule event function in normal conditions. """
+    with app.test_request_context('/'):
+        current_user = User.query.filter_by(username='ivan').first()
+        event = ScheduleEvent(
+            timestamp=datetime.datetime.now() - datetime.timedelta(hours=2),
+            start_timestamp=datetime.datetime.now() + datetime.timedelta(hours=4),
+            end_timestamp=datetime.datetime.now() + datetime.timedelta(hours=6),
+            user_id=current_user.id
+        )
+        db.session.add(event)
+        db.session.commit()
+        db.session.refresh(event)
+
+        with patch.object(ScheduleEvent, 'notification_task', return_value='task id') as mock_notification_task:
+            schedule_delete_event(event_id=event.id, user=current_user)
+            assert mock_notification_task.terminate.called
+
+        assert ScheduleEvent.query.get(event.id) is None
+        assert mock_send_push.call_count == 1
+
+
+@patch('app.functions.send_push_to_user')
+@patch('app.functions.redirect')
+@patch('app.functions.flash')
+def test_schedule_delete_event_invalid_id(mock_flash, mock_redirect, mock_send_push, app):
+    """ Testing the delete schedule event function with invalid event id. """
+    with app.test_request_context('/'):
+        current_user = User.query.filter_by(username='ivan').first()
+        event_id = 1
+        with patch.object(ScheduleEvent, 'notification_task', return_value='task id') as mock_notification_task:
+            schedule_delete_event(event_id=event_id, user=current_user)
+            assert not mock_notification_task.terminate.called
+
+        assert ScheduleEvent.query.get(event_id) is None
+        mock_flash.assert_called_with('Event not found', category='toast-error')
+        assert mock_redirect.called
+        assert mock_send_push.call_count == 0
+
+
+@patch('app.functions.send_push_to_user')
+@patch('app.functions.redirect')
+@patch('app.functions.flash')
+def test_schedule_delete_event_wrong_user(mock_flash, mock_redirect, mock_send_push, app):
+    """ Testing the delete schedule event function in normal conditions. """
+    with app.test_request_context('/'):
+        current_user = User.query.filter_by(username='ivan').first()
+        event_user = User.query.filter_by(username='andrei').first()
+        event = ScheduleEvent(
+            timestamp=datetime.datetime.now() - datetime.timedelta(hours=2),
+            start_timestamp=datetime.datetime.now() + datetime.timedelta(hours=4),
+            end_timestamp=datetime.datetime.now() + datetime.timedelta(hours=6),
+            user_id=event_user.id
+        )
+        db.session.add(event)
+        db.session.commit()
+        db.session.refresh(event)
+
+        with patch.object(ScheduleEvent, 'notification_task', return_value='task id') as mock_notification_task:
+            schedule_delete_event(event_id=event.id, user=current_user)
+            assert not mock_notification_task.terminate.called
+
+        assert ScheduleEvent.query.get(event.id) is not None
+        mock_flash.assert_called_with('You can only delete your own events', category='toast-error')
+        assert mock_redirect.called
+        assert mock_send_push.call_count == 0
