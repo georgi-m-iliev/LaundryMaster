71a323257169f11a35a185b52226843f1b69e7bf
Georgi Iliev <georgi.iliev533@outlook.com>
2024-02-23 20:05:45 +0200

    Fix tests by adding needed config entries

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 import os
 import pytest
 import tempfile
-from dotenv import load_dotenv
 from sqlalchemy.sql import text
 
 from app import create_app
@@ -9,8 +8,6 @@ from app.db import db
 
 with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
     _data_sql = f.read().decode('utf8')
-
-load_dotenv()
 
 
 @pytest.fixture
@@ -21,10 +18,15 @@ def app():
         TESTING = True
         SECRET_KEY = 'test91b801204a43803a3391979a'
         SECURITY_PASSWORD_SALT = 'dfWxHGdVuLwU'
+        SECURITY_AUTH_SALT = 'dfWxHGdVuLwU'
         SQLALCHEMY_DATABASE_URI = f'sqlite:///{db_path}'
         WTF_CSRF_ENABLED = False
+        FLASK_PUSH_PUBLIC_KEY = 'BKPkSUet9K8E1JME4PTlvtdbkuj1LNcYdDkQMkOYx6nDJ5ehzo2Ger_8JpNcgjOm_QSsc4nOf20VuMUgISeFCRc'
+        FLASK_PUSH_PRIVATE_KEY = 'NNxeRtRiQ_CcyPmIntk2QbQ7Apnav8JbYPq3R7xrMrw'
+        FLASK_PUSH_CLAIM_EMAIL = 'admin@localhost'
 
     app = create_app(TestConfig)
+    # os.environ['']
 
     with app.app_context():
         db.create_all()
